6aaae8b0e74e1201663dc4f69aadea87ea686f07
April Copes <antdevelopment1@gmail.com>
2018-09-28 22:21:26 -0400

    Building API from scratch in node js part 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// Primary file for API.
+
+// Dependencies
+var http = require('http');
+
+// The server should respond to all requests with a string.
+var server = http.createServer(function(req, res) {
+    res.end("Hello world\n");
+});
+
+// Start the server, and have it listen on port 3000.
+server.listen(3000, function() {
+    console.log("the server is listening on port 3000 now.")
+});
